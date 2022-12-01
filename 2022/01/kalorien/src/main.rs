0bea43e2520fc82fab7dc9adfbe6609e79eafcce
Huluvu424242 <huluvu424242@gmail.com>
2022-12-01 12:12:54 +0000

    projet setup für day 1 erstellt

---
+++ 2022/01/kalorien/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let input: String= """
+    1000
+    2000
+    3000
+
+    4000
+
+    5000
+    6000
+
+    7000
+    8000
+    9000
+
+    10000
+    """;
+    // TODO here alg implementieren
+}
