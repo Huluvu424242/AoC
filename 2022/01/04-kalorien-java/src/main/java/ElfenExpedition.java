c60f038648240788e9675539c1a808e5fceeccdd
huluvu424242 <huluvu424242@gmail.com>
2022-12-01 23:35:50 +0100

    [day1] teil 2 lösung refactoring
    Signed-off-by: huluvu424242 <huluvu424242@gmail.com>

--- 2022/01/04-kalorien-java/src/main/java/ElfenExpedition.java
+++ 2022/01/04-kalorien-java/src/main/java/ElfenExpedition.java
@@ -10,26 +10,33 @@ public class ElfenExpedition {
         elfenExpedition.berechne();
     }
 
-    public static class Elf {
+    public static class Elf implements Comparable<Elf> {
 
         int nr = 0;
         int cal = 0;
-        public Elf(int nr, int kalorien){
-            this.nr =nr;
-            this.cal =kalorien;
-        };
 
-        public String toString(){
-            return "\nElf Nr: "+this.nr + " trägt "+this.cal+ " Kalorien. ";
+        public Elf(int nr, int kalorien) {
+            this.nr = nr;
+            this.cal = kalorien;
         }
 
+        ;
+
+        public String toString() {
+            return "\nElf Nr: " + this.nr + " trägt " + this.cal + " Kalorien. ";
+        }
+
+        @Override
+        public int compareTo(Elf o) {
+            return Integer.compare(this.cal, o.cal);
+        }
     }
 
     /**
      * key: elfNr
      * value: kalorien
      */
-    protected Map<Integer,Integer> elfenMap = new HashMap<>();
+    protected Map<Integer, Integer> elfenMap = new HashMap<>();
 
     protected int curElfNr = 0;
 
@@ -49,29 +56,31 @@ public class ElfenExpedition {
     protected void auswertung() {
         final List<Elf> elfen = this.elfenMap.entrySet().stream()
             .map(entry -> new Elf(entry.getKey(), entry.getValue()))
-            .map(entry->{System.out.println(entry);return entry;})
-            .sorted(Comparator.comparingInt(o -> o.cal)).toList();
+            .peek(System.out::println)
+            .sorted(Collections.reverseOrder())
+            .toList();
 
-        final Elf firstElf = elfen.get(elfen.size()-1);
-        final Elf secondElf = elfen.get(elfen.size()-2);
-        final Elf thirdElf = elfen.get(elfen.size()-3);
-        System.out.format("\n\nDer Elf mit den meisten Kalorien ist Elf Nr: %d mit %d Kalorien",firstElf.nr,firstElf.cal);
-        System.out.format("\n\nDer Elf mit den meisten Kalorien ist Elf Nr: %d mit %d Kalorien",secondElf.nr,secondElf.cal);
-        System.out.format("\n\nDer Elf mit den meisten Kalorien ist Elf Nr: %d mit %d Kalorien",thirdElf.nr,thirdElf.cal);
+        final Elf firstElf = elfen.get(0);
+        final Elf secondElf = elfen.get(1);
+        final Elf thirdElf = elfen.get(2);
+        System.out.format("\n\nDer Elf mit den meisten Kalorien ist Elf Nr: %d mit %d Kalorien", firstElf.nr, firstElf.cal);
+        System.out.format("\n\nDer Elf mit den meisten Kalorien ist Elf Nr: %d mit %d Kalorien", secondElf.nr, secondElf.cal);
+        System.out.format("\n\nDer Elf mit den meisten Kalorien ist Elf Nr: %d mit %d Kalorien", thirdElf.nr, thirdElf.cal);
 
-        System.out.format("\n\nDie Top 3 Elfen tragen zusammen %d Kalorien.", (firstElf.cal+secondElf.cal+thirdElf.cal));
+        System.out.format("\n\nDie Top 3 Elfen tragen zusammen %d Kalorien.", (firstElf.cal + secondElf.cal + thirdElf.cal));
 
     }
+
     protected void erfassung(String line) {
         final String trimmedLine = line.trim();
         if (trimmedLine.length() > 0) {
             final Integer aufzunehmendeKalorien = Integer.parseInt(trimmedLine);
-            final Integer curKalorien= this.elfenMap.getOrDefault(this.curElfNr, 0);
+            final Integer curKalorien = this.elfenMap.getOrDefault(this.curElfNr, 0);
             final Integer neuKalorien = curKalorien + aufzunehmendeKalorien;
-            this.elfenMap.put(this.curElfNr,neuKalorien);
-            System.out.format("\n    Elf %d lädt %d Kalorien",this.curElfNr,  aufzunehmendeKalorien);
+            this.elfenMap.put(this.curElfNr, neuKalorien);
+            System.out.format("\n    Elf %d lädt %d Kalorien", this.curElfNr, aufzunehmendeKalorien);
         } else {
-            System.out.format("\n### Elf %d trägt %d Kalorien",this.curElfNr,  this.elfenMap.getOrDefault(this.curElfNr,0));
+            System.out.format("\n### Elf %d trägt %d Kalorien", this.curElfNr, this.elfenMap.getOrDefault(this.curElfNr, 0));
             this.curElfNr++;
         }
     }
