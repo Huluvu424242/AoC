0781edf80333a28fd2e47c14f0fff44585056354
Huluvu424242 <huluvu424242@gmail.com>
2022-12-01 18:25:45 +0000

    einlesen file

--- 2022/01/kalorien/src/main.rs
+++ 2022/01/kalorien/src/main.rs
@@ -1,19 +1,25 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
 fn main() {
-    let input: String= """
-    1000
-    2000
-    3000
 
-    4000
+    if let Ok(lines) = read_lines("./input.txt") {
+        println!("{:?}",lines);
+        for line in lines {
+            if let Ok(ip) = line {
+                println!(">{}", ip);
+            }
+        }
+    }
 
-    5000
-    6000
 
-    7000
-    8000
-    9000
 
-    10000
-    """;
-    // TODO here alg implementieren
+
 }
