bd981236d38c02e4b418002f5f34ff548b881e86
huluvu424242 <huluvu424242@gmail.com>
2022-12-06 23:11:15 +0100

    [day5] aufgabe teil1 - demo input works
    Signed-off-by: huluvu424242 <huluvu424242@gmail.com>

--- 2022/05/stapel-java/src/main/java/Stapel.java
+++ 2022/05/stapel-java/src/main/java/Stapel.java
@@ -1,6 +1,8 @@
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Stapel {
 
@@ -32,7 +34,7 @@ public class Stapel {
                     umdrehen(stacks);
                     printStacksVertical(stacks);
                 } else {
-                    executeAction(lineNr, zeile);
+                    executeAction(stacks,lineNr, zeile);
                 }
 
                 lineNr++;
@@ -43,16 +45,41 @@ public class Stapel {
         }
     }
 
-    protected void executeAction(final int zeilenNr, final String zeile) {
+    protected void executeAction(final Map<Integer, Haufen> stacks, final int zeilenNr, final String zeile) {
         System.out.format("(%d): %s\n", zeilenNr, zeile);
+        final String pattern = "move (\\d+) from (\\d+) to (\\d+)";
+        final Pattern r = Pattern.compile(pattern);
+        final Matcher m = r.matcher(zeile);
+        if (m.find()) {
+            final String group1 = m.group(1);
+            final String group2 = m.group(2);
+            final String group3 = m.group(3);
+
+            final int anzahl = Integer.parseInt(group1);
+            final int vonNr = Integer.parseInt(group2);
+            final int zuNr = Integer.parseInt(group3);
+            System.out.format("Verschiebe %d von %d zu %d.\n",anzahl,vonNr,zuNr);
+
+            final Haufen fromHaufen = stacks.get(vonNr-1);
+            final Haufen toHaufen = stacks.get(zuNr-1);
+            for (int i = 0; i < anzahl; i++) {
+                toHaufen.push(fromHaufen.pop());
+            }
+            stacks.put(vonNr-1,fromHaufen);
+            stacks.put(zuNr-1,toHaufen);
+            printStacksVertical(stacks);
+        }
+
     }
 
     protected void erfassung(final Map<Integer, Haufen> stacks, final String line) {
         int stackNr = 0;
         for (char zeichen : line.toCharArray()) {
             final Haufen stack = stacks.getOrDefault(stackNr, new Haufen());
-            stack.push(zeichen);
-            stacks.put(stackNr, stack);
+            if(!Character.isSpaceChar(zeichen)) {
+                stack.push(zeichen);
+                stacks.put(stackNr, stack);
+            }
             stackNr++;
         }
     }
@@ -74,7 +101,7 @@ public class Stapel {
     protected void printStacksVertical(final Map<Integer, Haufen> stacks) {
         for (int haufenIndex = 0; haufenIndex < stacks.size(); haufenIndex++) {
             final Haufen stack = stacks.get(haufenIndex);
-            System.out.format("|%d|:%s\n", (haufenIndex + 1), stack.toString());
+            System.out.format("|%d|:%s<\n", (haufenIndex + 1), stack.toString());
         }
     }
 
