95958ed8a06af47b26f81c9422e61f7a47494483
huluvu424242 <huluvu424242@gmail.com>
2022-12-10 19:59:59 +0100

    [day7] aufgabe teil1 - pfade erzeugen
    Signed-off-by: huluvu424242 <huluvu424242@gmail.com>

--- 2022/07/calculator-java/src/main/java/Calculator.java
+++ 2022/07/calculator-java/src/main/java/Calculator.java
@@ -1,8 +1,14 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
+
+    protected File curPath;
+
 
 
     public static void main(String[] args) {
@@ -17,15 +23,37 @@ public class Calculator {
             int lineNr = 1;
             while (scanner.hasNext()) {
                 final String zeile = scanner.nextLine();
-//                final int posPacketMarker = getMarkerPosFrom(zeile,4);
-//                final int posMessageMarker = getMarkerPosFrom(zeile,14);
-                System.out.format("\n[%d]: %s -> ", lineNr, zeile);
+                bearbeiteZeile(zeile);
+                System.out.format("\n[%d]: %s ->\n%s [%d]", lineNr, zeile,this.curPath.toString(),0);
 
                 lineNr++;
             }
 
         } catch (FileNotFoundException fnfe) {
             fnfe.printStackTrace();
+        }
+        // auswertung
+//        File.separator
+    }
+
+    private void bearbeiteZeile(final String zeile) {
+        if(zeile.matches("\\$ cd /")){
+            this.curPath=new File("/");
+            return;
+        }
+        if(zeile.matches("\\$ cd ..")){
+            this.curPath=this.curPath.getParentFile();
+            return;
+        }
+        if(zeile.matches("\\$ cd .*")){
+            final String pattern = "\\$ cd (.*)";
+            final Pattern r = Pattern.compile(pattern);
+            final Matcher m = r.matcher(zeile);
+            if (m.find()) {
+                final String folderName = m.group(1);
+                this.curPath =new File(curPath, folderName);
+            }
+            return;
         }
     }
 }
