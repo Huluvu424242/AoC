f707558dac84f4190020677591c50dc274229c73
huluvu424242 <huluvu424242@gmail.com>
2022-12-17 18:37:05 +0100

    [day8] aufgabe teil1 - daten erfasst
    Signed-off-by: huluvu424242 <huluvu424242@gmail.com>

--- 2022/08/baumhaus-java/src/main/java/day8/Main.java
+++ 2022/08/baumhaus-java/src/main/java/day8/Main.java
@@ -6,14 +6,12 @@ import java.nio.file.Path;
 
 public class Main {
 
-
-    public static void loese(final String folderName, final String inputFileName ) {
+    public void loese(final String folderName, final String inputFileName ) {
         final Path rootPath = Path.of(folderName);
         final Path inputPath = Path.of(rootPath.toString(), inputFileName);
         System.out.format("\n\nPath: %s", inputPath.toAbsolutePath());
         final Scanner scanner = new Scanner(new Algorithmus());
         scanner.erfasseDaten(inputPath);
     }
-
 
 }
